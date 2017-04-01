d5cb11c942ee39c374d50d2940f56007508c48b0
yonilev <yonilevv@gmail.com>
2017-04-01 22:17:20 +0100

    learning rate decay

--- training.py
+++ training.py
@@ -1,6 +1,7 @@
 from siamese_models import *
 from keras.optimizers import Adam
-from keras.callbacks import EarlyStopping,ModelCheckpoint,ReduceLROnPlateau
+from keras.callbacks import EarlyStopping,ModelCheckpoint,ReduceLROnPlateau,Callback
+from keras import backend as K
 import pandas as pd
 from sklearn.metrics import log_loss
 import glob
@@ -24,11 +25,22 @@ def train_model(model,df_train,df_val,epochs,prefix,early_stopping_patience,redu
                                      monitor='val_binary_crossentropy', verbose=1,
                                      save_best_only=True,save_weights_only=True))
     callbacks.append(EarlyStopping(monitor='val_binary_crossentropy', patience=early_stopping_patience, verbose=1))
-    callbacks.append(ReduceLROnPlateau(monitor='val_binary_crossentropy', factor=0.1, patience=reduce_lr_patience))
+    callbacks.append(ReduceLROnPlateau(monitor='val_binary_crossentropy', factor=0.5, patience=reduce_lr_patience))
+    callbacks.append(LearningRateDecay(0.9))
 
     history = model.model.fit_generator(train_gen,train_steps,validation_data=val_gen,
                               validation_steps=val_steps,epochs=epochs,callbacks=callbacks)
     pd.DataFrame(history.history).to_csv('models/{}.history'.format(prefix),index=False)
+
+
+class LearningRateDecay(Callback):
+    def __init__(self,decay):
+        self.decay = decay
+
+    def on_epoch_end(self, epoch, logs={}):
+        optimizer = self.model.optimizer
+        lr = K.eval(optimizer.lr * self.decay)
+        K.set_value(optimizer.lr, lr)
 
 
 def params_search(experiments,epochs,nrows,early_stopping_patience,reduce_lr_patience):
@@ -63,7 +75,7 @@ def main():
     tokenizer = load(TOKENIZER_20K_10K)
     model = LSTMSiamese(tokenizer)
     df_train,df_val,_ = read_train()
-    train_model(model,df_train,df_val,50,'lstm_embeddings',3,0)
+    train_model(model,df_train,df_val,50,'lstm_embeddings2',2,0)
 
     # params_search(100,50,50000,2,1)
 
